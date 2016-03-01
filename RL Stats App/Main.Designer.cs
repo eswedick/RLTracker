fcdb3535724fc7fa288960da61c760e423546313
Evan <eswedick@gmail.com>
2016-03-01 01:05:59 -0500

    updating

---
+++ RL Stats App/Main.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace RL_Stats_App
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.dgStats = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgStats)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(748, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 38);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(46, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.Text = "Solo";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Location = new System.Drawing.Point(64, 38);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(64, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Doubles";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(134, 38);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(68, 17);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.Text = "Standard";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // dgStats
+            // 
+            this.dgStats.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgStats.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            this.dgStats.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgStats.Location = new System.Drawing.Point(12, 61);
+            this.dgStats.Name = "dgStats";
+            this.dgStats.Size = new System.Drawing.Size(748, 274);
+            this.dgStats.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 347);
+            this.Controls.Add(this.dgStats);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "RL Tracker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.dgStats)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.DataGridView dgStats;
+    }
+}
+
---
+++ RL Stats App/Main.cs
@@ -0,0 +1,392 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+
+namespace RL_Stats_App
+{
+    public partial class Main : Form
+    {
+        string[] files;
+        Bitmap bmp;
+        TesseractEngine ocr;
+        string tessdata;
+        DataTable results;
+
+        const int letterHeight = 49;
+
+        const int length = 75;
+        const int nameLength = 300;
+        const int scoreLength = 100;
+
+        // x where columns begin
+        const int nameX = 1090;
+        const int scoreX = 1445;
+        const int goalX = 1550;
+        const int assistX = 1640;
+        const int saveX = 1750;
+        const int shotX = 1840;
+
+        // y for where rows start
+        const int p1_1 = 451;
+        const int p2_1 = 610;
+
+        const int p1_2 = 420;
+        const int p2_2 = 480;
+        const int p3_2 = 605;
+        const int p4_2 = 665;
+
+        const int p1_3 = 1;
+        const int p2_3 = 1;
+        const int p3_3 = 1;
+        const int p4_3 = 1;
+        const int p5_3 = 1;
+        const int p6_3 = 1;
+
+        #region "Dictionary"
+        private Dictionary<string, Rect> zones = new Dictionary<string, Rect>(){
+            //solo
+            {"1_1_Name", new Rect(nameX, p1_1, nameLength, letterHeight)},
+            {"2_1_Name", new Rect(nameX, p2_1, nameLength, letterHeight)},
+            {"1_1_Score", new Rect(scoreX, p1_1, scoreLength, letterHeight)},
+            {"2_1_Score", new Rect(scoreX, p2_1, scoreLength, letterHeight)},
+            {"1_1_Goals", new Rect(goalX, p1_1, length, letterHeight)},
+            {"2_1_Goals", new Rect(goalX, p2_1, length, letterHeight)},
+            {"1_1_Assists", new Rect(assistX, p1_1, length, letterHeight)},
+            {"2_1_Assists", new Rect(assistX, p2_1, length, letterHeight)},
+            {"1_1_Saves", new Rect(saveX, p1_1, length, letterHeight)},
+            {"2_1_Saves", new Rect(saveX, p2_1, length, letterHeight)},
+            {"1_1_Shots", new Rect(shotX, p1_1, length, letterHeight)},
+            {"2_1_Shots", new Rect(shotX, p2_1, length, letterHeight)},
+        
+            //doubles
+            {"1_2_Name", new Rect(nameX, p1_2, nameLength, letterHeight)},
+            {"2_2_Name", new Rect(nameX, p2_2, nameLength, letterHeight)},
+            {"3_2_Name", new Rect(nameX, p3_2, nameLength, letterHeight)},
+            {"4_2_Name", new Rect(nameX, p4_2, nameLength, letterHeight)},
+            {"1_2_Score", new Rect(scoreX, p1_2, scoreLength, letterHeight)},
+            {"2_2_Score", new Rect(scoreX, p2_2, scoreLength, letterHeight)},
+            {"3_2_Score", new Rect(scoreX, p3_2, scoreLength, letterHeight)},
+            {"4_2_Score", new Rect(scoreX, p4_2, scoreLength, letterHeight)},
+            {"1_2_Goals", new Rect(goalX, p1_2, length, letterHeight)},
+            {"2_2_Goals", new Rect(goalX, p2_2, length, letterHeight)},
+            {"3_2_Goals", new Rect(goalX, p3_2, length, letterHeight)},
+            {"4_2_Goals", new Rect(goalX, p4_2, length, letterHeight)},
+            {"1_2_Assists", new Rect(assistX, p1_2, length, letterHeight)},
+            {"2_2_Assists", new Rect(assistX, p2_2, length, letterHeight)},
+            {"3_2_Assists", new Rect(assistX, p3_2, length, letterHeight)},
+            {"4_2_Assists", new Rect(assistX, p4_2, length, letterHeight)},
+            {"1_2_Saves", new Rect(saveX, p1_2, length, letterHeight)},
+            {"2_2_Saves", new Rect(saveX, p2_2, length, letterHeight)},
+            {"3_2_Saves", new Rect(saveX, p3_2, length, letterHeight)},
+            {"4_2_Saves", new Rect(saveX, p4_2, length, letterHeight)},
+            {"1_2_Shots", new Rect(shotX, p1_2, length, letterHeight)},
+            {"2_2_Shots", new Rect(shotX, p2_2, length, letterHeight)},
+            {"3_2_Shots", new Rect(shotX, p3_2, length, letterHeight)},
+            {"4_2_Shots", new Rect(shotX, p4_2, length, letterHeight)}
+
+            //standard
+        };
+        #endregion
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            results = new DataTable();
+            results.Columns.Add("player");
+            results.Columns.Add("rank");
+            results.Columns.Add("score");
+            results.Columns.Add("goals");
+            results.Columns.Add("assists");
+            results.Columns.Add("saves");
+            results.Columns.Add("shots");
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            run();
+        }
+
+        private void run()
+        {
+            results.Rows.Clear();
+
+            //get screenshot file path
+            textBox1.Text = files[0];
+
+            //image preprocessing
+            var outImage = processFile(files);
+
+            //get bitmap for ocr
+            bmp = new Bitmap(outImage);
+
+            //create ocr
+            tessdata = Application.StartupPath + "\\tessdata";
+            ocr = new TesseractEngine(tessdata, "eng", EngineMode.Default);
+
+            //call appropriate function to parse scores
+            if (radioButton1.Checked)
+            {
+                recognizeSolo(outImage);
+            }
+            else if (radioButton2.Checked)
+            {
+                recognizeDoubles(outImage);
+
+            }
+            else
+            {
+                recognizeStandard(outImage);
+            }
+        }
+
+        private string processFile(string[] files)
+        {
+            string image = files[0].Replace(".jpg", "_out.jpg");
+            var outImage = image;
+
+            //delete file if it exists
+            try
+            {
+                FileSystemInfo fsi = new FileInfo(image);
+                if (fsi.Exists)
+                {
+                    fsi.Delete();
+                }
+
+                var colorImage = AForge.Imaging.Image.FromFile(files[0]);
+                var grayscaleImage = AForge.Imaging.Filters.Grayscale.CommonAlgorithms.BT709.Apply(colorImage);
+                var invertedImage = new AForge.Imaging.Filters.Invert().Apply(grayscaleImage);
+                var threshImage = new AForge.Imaging.Filters.Threshold(145).Apply(invertedImage);
+
+                threshImage.Save(outImage);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return outImage;
+        }
+
+        private void recognizeSolo(string outImage)
+        {
+            //name
+            var player1Name = ocrProcess("1_1_Name");
+            var player2Name = ocrProcess("2_1_Name");
+
+            //rank
+            var player1Rank = ocrProcess("1_1_Name", false);
+            var player2Rank = ocrProcess("2_1_Name", false);
+
+            // start looking for just numbers //
+            ocr.SetVariable("tessedit_char_whitelist", "0123456789");
+
+            //score
+            var player1Score = ocrProcess("1_1_Score");
+            var player2Score = ocrProcess("2_1_Score");
+
+            // single character only //
+            ocr.DefaultPageSegMode = PageSegMode.SingleChar;
+
+            //goals
+            var player1Goals = ocrProcess("1_1_Goals");
+            var player2Goals = ocrProcess("2_1_Goals");
+
+            //assists
+            var player1assists = ocrProcess("1_1_Assists");
+            var player2assists = ocrProcess("2_1_Assists");
+
+            //saves
+            var player1saves = ocrProcess("1_1_Saves");
+            var player2saves = ocrProcess("2_1_Saves");
+
+            //shots
+            var player1shots = ocrProcess("1_1_Shots");
+            var player2shots = ocrProcess("2_1_Shots");
+
+            //add items to grid
+            results.Rows.Add(new object[] { player1Name, player1Rank, player1Score, player1Goals, player1assists, player1saves, player1shots });
+            results.Rows.Add(new object[] { player2Name, player2Rank, player2Score, player2Goals, player2assists, player2saves, player2shots });
+
+            //bind data
+            dgStats.DataSource = results;
+
+        }
+
+        private void recognizeDoubles(string outImage)
+        {
+            //name
+            var player1Name = ocrProcess("1_2_Name");
+            var player2Name = ocrProcess("2_2_Name");
+            var player3Name = ocrProcess("3_2_Name");
+            var player4Name = ocrProcess("4_2_Name");
+
+            //rank
+            var player1Rank = ocrProcess("1_2_Name", false);
+            var player2Rank = ocrProcess("2_2_Name", false);
+            var player3Rank = ocrProcess("3_2_Name", false);
+            var player4Rank = ocrProcess("4_2_Name", false);
+
+            // start looking for just numbers //
+            ocr.SetVariable("tessedit_char_whitelist", "0123456789");
+
+            //score
+            var player1Score = ocrProcess("1_2_Score");
+            var player2Score = ocrProcess("2_2_Score");
+            var player3Score = ocrProcess("3_2_Score");
+            var player4Score = ocrProcess("4_2_Score");
+
+            // single character only //
+            ocr.DefaultPageSegMode = PageSegMode.SingleChar;
+
+            //goals
+            var player1Goals = ocrProcess("1_2_Goals");
+            var player2Goals = ocrProcess("2_2_Goals");      
+            var player3Goals = ocrProcess("3_2_Goals");         
+            var player4Goals =  ocrProcess("4_2_Goals");
+
+            //assists
+            var player1assists = ocrProcess("1_2_Assists");
+            var player2assists = ocrProcess("2_2_Assists");
+            var player3assists = ocrProcess("3_2_Assists");         
+            var player4assists = ocrProcess("4_2_Assists");     
+
+            //saves
+            var player1saves = ocrProcess("1_2_Saves");
+            var player2saves = ocrProcess("2_2_Saves");
+            var player3saves = ocrProcess("3_2_Saves");      
+            var player4saves = ocrProcess("4_2_Saves");        
+
+            //shots
+            var player1shots = ocrProcess("1_2_Shots");
+            var player2shots = ocrProcess("2_2_Shots");
+            var player3shots = ocrProcess("3_2_Shots");
+            var player4shots = ocrProcess("4_2_Shots");
+
+            //add items to grid
+            results.Rows.Add(new object[] { player1Name, player1Rank, player1Score, player1Goals, player1assists, player1saves, player1shots });
+            results.Rows.Add(new object[] { player2Name, player2Rank, player2Score, player2Goals, player2assists, player2saves, player2shots });
+            results.Rows.Add(new object[] { player3Name, player3Rank, player3Score, player3Goals, player3assists, player3saves, player3shots });
+            results.Rows.Add(new object[] { player4Name, player4Rank, player4Score, player4Goals, player4assists, player4saves, player4shots });
+
+            //bind data
+            dgStats.DataSource = results;
+        }
+
+        private void recognizeStandard(string outImage)
+        {
+            Bitmap bmp = new Bitmap(outImage);
+            var tessdata = Application.StartupPath + "\\tessdata";
+            var ocr = new TesseractEngine(tessdata, "eng", EngineMode.Default);
+
+            //var process = ocr.Process(bmp, player1NameRect);
+            //var player1Info = process.GetText().Trim().Split('\n');
+            //process.Dispose();
+
+            //process = ocr.Process(bmp, player2NameRect);
+            //var player2Info = process.GetText().Trim().Split('\n');
+            //process.Dispose();
+
+            //player1Name = player1Info[0];
+            //player2Name = player2Info[0];
+            //if (player1Info.Length == 1)
+            //    player1Rank = "";
+            //else
+            //    player1Rank = player1Info[1];
+            //if (player2Info.Length == 1)
+            //    player2Rank = "";
+            //else
+            //    player2Rank = player2Info[1];
+
+            //ocr.SetVariable("tessedit_char_whitelist", "0123456789");
+
+            //process = ocr.Process(bmp, player1ScoreRect);
+            //var player1Score = process.GetText().Trim();
+            //process.Dispose();
+
+            //process = ocr.Process(bmp, player2ScoreRect);
+            //var player2Score = process.GetText().Trim();
+            //process.Dispose();
+
+            //process = ocr.Process(bmp, player1GoalsRect);
+            //var player1Goals = process.GetText().Trim();
+            //process.Dispose();
+
+            //process = ocr.Process(bmp, player2GoalsRect);
+            //var player2Goals = process.GetText().Trim();
+            //process.Dispose();
+
+            //dgStats.Items.Add(new ListViewItem(new string[]
+            //{
+            //    player1Name,
+            //    player1Rank,
+            //    player1Score,
+            //    player1Goals
+            //}));
+            //dgStats.Items.Add(new ListViewItem(new string[]
+            //{
+            //    player2Name,
+            //    player2Rank,
+            //    player2Score,
+            //    player2Goals
+            //}));
+            Console.WriteLine("Jones");
+        }
+
+        private string ocrProcess(string zone, Boolean returnFirst = true)
+        {
+            var data = "";
+            var process = ocr.Process(bmp, zones[zone]);
+            if (zone.Contains("Name"))
+            {
+                data = SplitNameRank(process.GetText().Trim().Split('\n'), returnFirst);
+            }
+            else
+            {
+                data = process.GetText().Trim();
+            }
+            process.Dispose();
+
+            return data;
+
+        }
+
+        private string SplitNameRank(string[] data, Boolean ReturnName)
+        {
+            if (ReturnName)
+            {
+                return data[0];
+            }
+            else
+            {
+                if (data.Count<string>() > 1)
+                {
+                    return data[1];
+                }
+                else
+                {
+                    return "";
+                }
+            }
+
+        }
+
+    }
+}
